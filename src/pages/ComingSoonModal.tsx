8c4732bff8d4a1283d0f3ee25ca4e9ce2f1466cd
wizo <westwizo@yahoo.com>
2024-11-21 01:55:50 +0000

    fint

--- src/pages/ComingSoonModal.tsx
+++ src/pages/ComingSoonModal.tsx
@@ -136,7 +136,7 @@ const ComingSoonModal: React.FC<ComingSoonModalProps> = ({ isOpen }) => {
         
         {/* Redirect "Got it" button to main page */}
         <button
-          onClick={() => window.location.href = '/'} 
+          onClick={() => window.location.href = 'https://soleer.xyz'} 
           className="mt-8 px-6 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700 transition-colors duration-200 font-semibold"
         >
           Got it
